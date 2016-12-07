cb1f554637c5269148535aa8081c703f3c8851bf
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-12-06 22:51:17 -0800

    got some code down, works so far

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const firebase = require('firebase');
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(firebaseConfig);
